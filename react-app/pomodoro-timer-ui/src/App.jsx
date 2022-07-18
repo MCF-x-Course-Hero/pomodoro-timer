d35677ac336d7fa85c3d454babf495d7dfd80bc2
JoseMario3 <josefolgar1232@gmail.com>
2022-07-18 11:05:00 -0500

    PomoZone Skeleton

---
+++ react-app/pomodoro-timer-ui/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>I am a timer</p>
+    </div>
+  )
+}
+
+export default App
