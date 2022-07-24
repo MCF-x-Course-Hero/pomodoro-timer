3cbc1fb8fde3cf871e996cf8fc7be09c0b1a2908
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-07-24 00:31:39 -0400

    css

--- react-app/pomodoro-timer-ui/src/Components/TodoTab/Todo.jsx
+++ react-app/pomodoro-timer-ui/src/Components/TodoTab/Todo.jsx
@@ -10,10 +10,7 @@ import {
   Typography,
 } from "@mui/material";
 
-
-
 export default function ({ todo, toggleComplete, removeTodo }) {
-  
   // this handler will evoke toggle complete whenever the respective todo's checkbox is clicked
   function handleCheckBoxClick() {
     toggleComplete(todo.id);
@@ -24,33 +21,35 @@ export default function ({ todo, toggleComplete, removeTodo }) {
   }
 
   return (
-    <List style={{ display: "flex" }}>
+    <List style={{ display: "flex"}}>
       <Checkbox
+        style={{marginRight:"4%"}}
+        className="check-box"
         checked={todo.is_completed}
         type="checkbox"
         onClick={handleCheckBoxClick}
       />
-      <Typography
-        variant="body1"
+      <p
+        // variant="body1"
         className="todo-text"
         style={{
           textDecoration: todo.is_completed ? "line-through" : null,
         }}
       >
         {todo.task}
-      </Typography>
+      </p>
       <div className="close-btn">
-      <IconButton  onClick={handleRemoveButton}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          viewBox="0 0 24 24"
-          width="24"
-        >
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-        </svg>
-      </IconButton>
+        <IconButton onClick={handleRemoveButton}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 0 24 24"
+            width="24"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
+        </IconButton>
       </div>
     </List>
   );
