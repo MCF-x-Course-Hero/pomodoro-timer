dbba268484c79f0a481477c477996f329be140a0
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-07-25 18:24:33 -0400

    fixed export in app.jsx

--- react-app/pomodoro-timer-ui/src/Components/App/App.jsx
+++ react-app/pomodoro-timer-ui/src/Components/App/App.jsx
@@ -17,7 +17,6 @@ function App() {
   const {width, height} = useWindowSize();
   const { settingsStates } = useSettingsContext();
 
-export default function App() {
   return (
       <div className={`app ${settingsStates.session}-${settingsStates.theme}`}>
         {settingsStates.isExploding ? <Confetti
