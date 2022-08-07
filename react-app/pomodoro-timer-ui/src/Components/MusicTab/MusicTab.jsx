5d2b4ec0c95552a8cf39255fd7859c338b09109e
mariamconde <condemriam@gmail.com>
2022-08-06 23:26:23 -0400

    link to spotify

--- react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
+++ react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
@@ -7,7 +7,8 @@ export default function MusicTab() {
   return (
     <div className="music-tab">
       <h1>Music</h1>
-
+      <div className="subtitle">click to improve your mood and increase memorization</div>
+      <a href="https://open.spotify.com/playlist/0vvXsWCC9xrXsKd4FyS8kM">Spotify</a>
     </div>
   );
 }
