bf8ad6112be1d922388cd6883245c41c938f2f3a
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-08-08 03:34:09 -0400

    PinnedTodo looks presentable

--- react-app/pomodoro-timer-ui/src/Components/PinnedTodo/PinnedTodo.jsx
+++ react-app/pomodoro-timer-ui/src/Components/PinnedTodo/PinnedTodo.jsx
@@ -27,14 +27,14 @@ export default function PinnedTodo() {
 
   return (
       <div className={pinnedTodo.task ? "pinned-task-content" : ""}>
-        {pinnedTodo.task? <svg
+        
+        {pinnedTodo.task?<div className="pin"> <svg
           onClick={handlePinButton}
           xmlns="http://www.w3.org/2000/svg"
-          className="pin"
           width="32"
           height="32"
           viewBox="0 0 24 24"
-          strokeWidth="2"
+          strokeWidth="1.75"
           stroke="#000000"
           fill="none"
           strokeLinecap="round"
@@ -44,7 +44,7 @@ export default function PinnedTodo() {
           <path d="M15 4.5l-4 4l-4 1.5l-1.5 1.5l7 7l1.5 -1.5l1.5 -4l4 -4" />
           <line x1="9" y1="15" x2="4.5" y2="19.5" />
           <line x1="14.5" y1="4" x2="20" y2="9.5" />
-        </svg> : ""}
+        </svg> </div>: ""}
         <p className="pinned-task">{pinnedTodo.task ? pinnedTodo.task : ""}</p>
     </div>
   );
