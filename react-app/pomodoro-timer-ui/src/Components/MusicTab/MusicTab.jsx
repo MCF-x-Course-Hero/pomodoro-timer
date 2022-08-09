f2d0d5646a4de716624bb83a93dcc1c26058387a
mariamconde <condemriam@gmail.com>
2022-08-09 12:18:49 -0400

    music tab

--- react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
+++ react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import "./MusicTab.css";
+import ReactPlayer from 'react-player';
+
 
 export default function MusicTab() {
-
-  
   return (
     <div className="music-tab">
       <h1>Music</h1>
-      <div className="subtitle">click to improve your mood and increase memorization</div>
-      <a href="https://open.spotify.com/playlist/0vvXsWCC9xrXsKd4FyS8kM">Spotify</a>
     </div>
   );
 }
