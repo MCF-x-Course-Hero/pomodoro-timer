7b2bb893cac21079d6f075477684ba9916237380
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-08-10 17:37:35 -0400

    VideoPlayer works with Playlists now

--- react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
+++ react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
@@ -1,18 +1,35 @@
 import * as React from "react";
 import "./MusicTab.css"
 import ReactPlayer from 'react-player/youtube'
-
-import VideoPlayer from "./VideoPlayer";
+import {useMusicContext} from "../../contexts/MusicContext"
 import MusicForm from "./MusicForm";
 import MusicIcon from "./MusicIcon";
+
 export default function MusicTab({displayType}){
+  const { MusicContextVariables } = useMusicContext();
+  const urlForm  = MusicContextVariables.urlForm;
   
     return (
       <div className="music-tab" style={{display:displayType}}>
       <h3>Music </h3>
         <MusicForm/>
-        <VideoPlayer displayType={displayType}/>
+        <VideoPlayer key = {String(urlForm)} displayType={displayType} urlForm={urlForm}/>
         <MusicIcon/>
       </div>
     )
 }
+
+export function VideoPlayer({ displayType, urlForm }) {
+
+  return (
+    <div className="video" style={{ display: displayType }}>
+      <ReactPlayer
+        width="410px"
+        url={urlForm}
+        controls
+        playing={true}
+      />
+    </div>
+  );
+}
+
