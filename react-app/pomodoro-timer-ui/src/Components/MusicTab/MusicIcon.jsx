52a8b237b224a5a3ea0aad21f1943a89d700d19a
mariamconde <condemriam@gmail.com>
2022-08-11 00:33:25 -0400

    format with prettier and removed icons

--- react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicIcon.jsx
+++ react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicIcon.jsx
@@ -18,7 +18,7 @@ export default function MusicIcon() {
             handleOnSubmitIcon("https://youtu.be/jfKfPfyJRdk");
           }}
         >
-          LOFI 
+          LO-FI
         </button>
         <button
           id="btnplay"
@@ -26,7 +26,7 @@ export default function MusicIcon() {
             handleOnSubmitIcon("https://youtu.be/neV3EPgvZ3g");
           }}
         >
-          JAZZ <i class="fa-solid fa-record-vinyl"></i>
+          JAZZ
         </button>
       </div>
       <div className="button2">
@@ -37,21 +37,6 @@ export default function MusicIcon() {
           }}
         >
           FIRE PLACE{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-flame"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#ffffff"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 12c2 -2.96 0 -7 -1 -8c0 3.038 -1.773 4.741 -3 6c-1.226 1.26 -2 3.24 -2 5a6 6 0 1 0 12 0c0 -1.532 -1.056 -3.94 -2 -5c-1.786 3 -2.791 3 -4 2z" />
-          </svg>
         </button>
         <button
           id="btnplay"
@@ -60,23 +45,6 @@ export default function MusicIcon() {
           }}
         >
           OCEAN WAVES{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-ripple"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#ffffff"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M3 7c3 -2 6 -2 9 0s6 2 9 0" />
-            <path d="M3 17c3 -2 6 -2 9 0s6 2 9 0" />
-            <path d="M3 12c3 -2 6 -2 9 0s6 2 9 0" />
-          </svg>
         </button>
       </div>
       <div className="button3">
@@ -87,22 +55,6 @@ export default function MusicIcon() {
           }}
         >
           RAIN{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-cloud-rain"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#ffffff"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" />
-            <path d="M11 13v2m0 3v2m4 -5v2m0 3v2" />
-          </svg>
         </button>
         <button
           id="btnplay"
@@ -111,22 +63,6 @@ export default function MusicIcon() {
           }}
         >
           BIRD{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-feather"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#ffffff"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M4 20l10 -10m0 -5v5h5m-9 -1v5h5m-9 -1v5h5m-5 -5l4 -4l4 -4" />
-            <path d="M19 10c.638 -.636 1 -1.515 1 -2.486a3.515 3.515 0 0 0 -3.517 -3.514c-.97 0 -1.847 .367 -2.483 1m-3 13l4 -4l4 -4" />
-          </svg>
         </button>
       </div>
     </div>
--- react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
+++ react-app/pomodoro-timer-ui/src/Components/MusicTab/MusicTab.jsx
@@ -1,36 +1,33 @@
 import * as React from "react";
-import "./MusicTab.css"
-import ReactPlayer from 'react-player/youtube'
-import {useMusicContext} from "../../contexts/MusicContext"
+import "./MusicTab.css";
+import ReactPlayer from "react-player/youtube";
+import { useMusicContext } from "../../contexts/MusicContext";
 import MusicForm from "./MusicForm";
 import MusicIcon from "./MusicIcon";
 
-export default function MusicTab({displayType}){
+export default function MusicTab({ displayType }) {
   const { MusicContextVariables } = useMusicContext();
-  const urlForm  = MusicContextVariables.urlForm;
-  
-    return (
-      <div className="music-tab" style={{display:displayType}}>
-      <h3>Music </h3>
-      <p>To help improve your mood and increase productivity!</p>
-        <MusicForm/>
-        <VideoPlayer key = {String(urlForm)} displayType={displayType} urlForm={urlForm}/>
-        <MusicIcon/>
-      </div>
-    )
-}
-
-export function VideoPlayer({ displayType, urlForm }) {
+  const urlForm = MusicContextVariables.urlForm;
 
   return (
-    <div className="video" style={{ display: displayType }}>
-      <ReactPlayer
-        width="410px"
-        url={urlForm}
-        controls
-        playing={true}
+    <div className="music-tab" style={{ display: displayType }}>
+      <h3>Music </h3>
+      <p>To help improve your mood and increase productivity!</p>
+      <MusicForm />
+      <VideoPlayer
+        key={String(urlForm)}
+        displayType={displayType}
+        urlForm={urlForm}
       />
+      <MusicIcon />
     </div>
   );
 }
 
+export function VideoPlayer({ displayType, urlForm }) {
+  return (
+    <div className="video" style={{ display: displayType }}>
+      <ReactPlayer width="410px" url={urlForm} controls playing={true} />
+    </div>
+  );
+}
