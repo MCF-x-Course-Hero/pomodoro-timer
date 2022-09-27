860f64ac7785b643b5e1dd6981ae32d24c5682b6
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-09-27 01:00:23 -0400

    Update auth.js
    removed console.log

--- react-app/pomodoro-timer-api/routes/auth.js
+++ react-app/pomodoro-timer-api/routes/auth.js
@@ -12,7 +12,6 @@ router.post("/login", async (req, res, next) => {
   try {
     const user = await User.login(req.body);
     const token = createUserJwt(user);
-    console.log("created token: ", token)
     return res.status(201).json({ user, token });
   } catch (err) {
     next(err);
