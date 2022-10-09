6700be754d1e89b4634a16b1b137c784c49fc128
JoseMario3 <josefolgar1232@gmail.com>
2022-10-09 17:51:42 -0500

    edited login

--- react-app/pomodoro-timer-ui/src/Components/SessionHistory/SessionHistory.jsx
+++ react-app/pomodoro-timer-ui/src/Components/SessionHistory/SessionHistory.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import apiClient from "../../Services/ApiClient.js";
+import apiClient from "../../Services/ApiClient";
 import { useAuthContext } from "../../contexts/AuthContext";
 import "./SessionHistory.css";
 
